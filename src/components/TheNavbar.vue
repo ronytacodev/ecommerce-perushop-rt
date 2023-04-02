6e2112989f98495a84f8a0bc4f81e873e259751b
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-02 00:53:43 -0500

    add images , starter code component Navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app-bar app color="#3853D8" dark>
+        <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-divider vertical class="ml-1 mr-3"></v-divider>
+        <v-btn icon>
+            <v-icon>mdi-menu</v-icon>
+        </v-btn>
+        <v-toolbar-title>
+            <span class="caption">Select</span><br>Category
+        </v-toolbar-title>
+        <v-divider vertical class="ml-5"></v-divider>
+        <div style="position:absolute; margin-left:auto; margin-right:auto; left:0; right:0; text-align:center;">
+            <h4>PeruShop R&T</h4>
+        </div>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+  <v-app>
+    <TheNavbar class="index" />
+    <TheDashboard />
+    <TheFooter />
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import TheDashboard from '@/components/TheDashboard.vue';
+import TheFooter from '@/components/TheFooter.vue';
+import TheNavbar from '@/components/TheNavbar.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    TheNavbar,
+    TheDashboard,
+    TheFooter
+  }
+}
+</script>
+
+<style>
+.index {
+  z-index: 9999;
+}
+</style>
