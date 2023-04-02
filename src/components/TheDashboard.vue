1cd0a7d718cfac4b75fd22cfae1ac36b9a077ee5
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-02 12:45:43 -0500

    add products sneakers to Dashboard

--- src/components/TheDashboard.vue
+++ src/components/TheDashboard.vue
@@ -193,6 +193,37 @@
                         </v-chip-group>
                     </v-card>
                 </v-col>
+                <v-col cols="9" class="mt-n3">
+                    <v-row>
+                        <v-col cols="12" sm="4" v-for="(clothe, i) in clothes" :key="i" :class="clothe.class">
+                            <v-hover v-slot:default="{ hover }">
+                                <v-card height="300" align="center" flat outlined tile>
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="black" small dark>{{ clothe.sold }}</v-btn>
+                                    </v-card-actions>
+                                    <v-img :src=clothe.image width="200" height="200" contain></v-img>
+                                    <v-card-text class="mt-n4">
+                                        <strong :class="hover ? 'red--text' : 'black--text'">
+                                            {{ clothe.title }}
+                                        </strong>
+                                    </v-card-text>
+                                    <v-expand-transition>
+                                        <div v-if="hover"
+                                            class="d-flex transition-fast-in-fast-out blue v-card--reveal display-3 white--text"
+                                            style="height: 100%;">
+                                            <v-btn rounded color="white">
+                                                <v-icon>mdi-cart-outlined</v-icon>
+                                                Buy
+                                            </v-btn>
+
+                                        </div>
+                                    </v-expand-transition>
+                                </v-card>
+                            </v-hover>
+                        </v-col>
+                    </v-row>
+                </v-col>
             </v-row>
         </v-card>
     </v-container>
@@ -281,6 +312,93 @@ export default {
         ],
         sizes: [
             '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46',
+        ],
+        clothes: [
+            {
+                class: 'pa-0',
+                sold: '-20%',
+                image: '1.png',
+                title: 'KD 8 EXT',
+                price: '$ 145.00',
+            },
+            {
+                class: 'pa-0',
+                sold: '-30%',
+                image: '2.png',
+                title: 'Jordan Galaxy',
+                price: '$ 599.00',
+            },
+            {
+                class: 'py-0 pl-0',
+                sold: '-17%',
+                image: '3.png',
+                title: 'Nike SB Trainerendor Leathe',
+                price: '$ 198.00',
+            },
+            {
+                class: 'pa-0',
+                sold: '-22%',
+                image: '4.png',
+                title: 'Air Jordan Spike 40 iD',
+                price: '$ 220.00',
+            },
+            {
+                class: 'pa-0',
+                sold: '-18%',
+                image: '5.png',
+                title: 'Nike Air Footscape Magista Flyknit',
+                price: '$ 235.00',
+            },
+            {
+                class: 'py-0 pl-0',
+                sold: '-40%',
+                image: '6.png',
+                title: 'Nike Air Zoom Huarache 2k4',
+                price: '$ 199.00',
+            },
+            {
+                class: 'pa-0',
+                sold: '-13%',
+                image: '7.png',
+                title: 'Jordan Horizon',
+                price: '$ 230.00',
+            },
+            {
+                class: 'pa-0',
+                sold: '-15%',
+                image: '8.png',
+                title: 'Air Jordan xx9 Low',
+                price: '$ 185.00',
+            },
+            {
+                class: 'py-0 pl-0',
+                sold: '-10%',
+                image: '9.png',
+                title: 'Lebron XIII Premium AS ID',
+                price: '$ 265.00',
+            },
+            {
+                class: 'pa-0',
+                sold: '-19%',
+                image: '10.png',
+                title: 'Air Jordan 1 Retro Hight Nouveau',
+                price: '$ 190.00',
+            },
+            {
+                class: 'pa-0',
+                sold: '-16%',
+                image: '11.png',
+                title: 'Nike Air Presto',
+                price: '$ 175.00',
+            },
+            {
+                class: 'py-0 pl-0',
+                sold: '-10%',
+                image: '12.png',
+                title: 'KD 8 Premium AS ID',
+                price: '$ 245.00',
+            },
+
         ],
     })
 }
