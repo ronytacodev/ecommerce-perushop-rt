a1fd36a3293bc5cc3b4c5dbc55f25d910f664ab5
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-02 11:15:48 -0500

    add row filter, sport by, show, compare dashboard

--- src/components/TheDashboard.vue
+++ src/components/TheDashboard.vue
@@ -57,6 +57,64 @@
                     </v-card>
                 </v-col>
             </v-row>
+            <v-row>
+                <v-col cols="12" sm="3" class="mt-n6 pr-0">
+                    <v-toolbar flat outlined>
+                        <v-toolbar-title><strong>filter</strong></v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-divider vertical></v-divider>
+                        <v-btn icon class="ml-1">
+                            <v-icon color="#3853D8">
+                                mdi-sync
+                            </v-icon>
+                        </v-btn>
+                    </v-toolbar>
+                </v-col>
+                <v-col cols="12" sm="3" class="mt-n6 px-0">
+                    <v-toolbar flat outlined>
+                        <v-toolbar-title>
+                            SPORT BY: <span>Price $-$$</span>
+                        </v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-btn icon class="mr-4">
+                            <v-icon color="black">mdi-menu-down</v-icon>
+                        </v-btn>
+                    </v-toolbar>
+                </v-col>
+                <v-col cols="12" sm="3" class="mt-n6 px-0">
+                    <v-toolbar flat outlined class="">
+                        <v-toolbar-title>SHOW: <span>12</span></v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-btn icon class="mr-4">
+                            <v-icon color="black">mdi-menu-down</v-icon>
+                        </v-btn>
+                    </v-toolbar>
+                </v-col>
+                <v-col cols="12" sm="3" class="mt-n6 pl-0">
+                    <v-toolbar flat outlined>
+                        <v-btn icon class="mr-1">
+                            <v-icon color="#3853D8">mdi-apps</v-icon>
+                        </v-btn>
+                        <v-divider vertical></v-divider>
+                        <v-btn icon class="mx-1">
+                            <v-icon color="grey">mdi-format-list-bulleted</v-icon>
+                        </v-btn>
+                        <v-divider vertical></v-divider>
+                        <v-toolbar-title class="ml-2">
+                            <strong>COMPARE: </strong>
+                        </v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-badge color="#3853D8" content="3" class="mr-2"></v-badge>
+                    </v-toolbar>
+                </v-col>
+                <v-col cols="3" class="py-0 pr-0 mt-n3">
+                    <v-card flat outlined tile>
+                        <v-toolbar flat>
+                            <v-icon color="black" class="mr-2">mdi-chevron-down</v-icon>
+                        </v-toolbar>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-card>
     </v-container>
 </template>
