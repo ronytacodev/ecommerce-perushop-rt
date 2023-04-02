87a48615957c4bda5d76647501fb3d559f1c8b45
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-02 01:04:37 -0500

    progress code Navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -14,6 +14,17 @@
         <div style="position:absolute; margin-left:auto; margin-right:auto; left:0; right:0; text-align:center;">
             <h4>PeruShop R&T</h4>
         </div>
+        <v-spacer></v-spacer>
+        <v-divider vertical></v-divider>
+        <v-btn icon class="mx-1">
+            <v-icon>mdi-account-outline</v-icon>
+        </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn icon class="mx-1">
+            <v-badge color="#94D0EF" content="2">
+                <v-icon>mdi-cart-outline</v-icon>
+            </v-badge>
+        </v-btn>
     </v-app-bar>
 </template>
 
@@ -23,4 +34,13 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.v-toolbar__title {
+    font-size: 1rem !important;
+}
+
+.v-badge__badge {
+    font-size: 10px !important;
+    /* me quede en el min 09.12 */
+}
+</style>
