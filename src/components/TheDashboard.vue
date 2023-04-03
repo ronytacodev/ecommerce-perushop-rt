be05858041906d808236d0c24b5b2b1cce6de753
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-03 07:38:57 -0500

    add newsletter

--- src/components/TheDashboard.vue
+++ src/components/TheDashboard.vue
@@ -245,6 +245,34 @@
                         <v-pagination v-model="page" :length="4"></v-pagination>
                     </v-toolbar>
                 </v-col>
+                <v-col cols="12" sm="3" class="mt-n3 pl-0 pb-0">
+                    <v-toolbar flat outlined>
+                        <v-btn icon class="mr-1">
+                            <v-icon color="grey">mdi-chevron-down</v-icon>
+                        </v-btn>
+                        <v-divider vertical></v-divider>
+                    </v-toolbar>
+                </v-col>
+            </v-row>
+        </v-card>
+        <v-card color="#EFF0F2" height="250px" tile flat dark class="d-flex align-center justify-center mt-n10 card2">
+            <v-row>
+                <v-col cols="12" sm="12">
+                    <h4 class="text-center black--text mt-10">SUBSCRIBE TO OUR NEWSLETTER</h4>
+                </v-col>
+                <v-col cols="12" sm="12">
+                    <h6 class="text-center grey--text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus obcaecati suscipit
+                        distinctio consequatur sed eveniet rerum, excepturi sequi maiores sit placeat tempora quod. Pariatur
+                        tempora, libero consequuntur a deserunt optio.
+                    </h6>
+                </v-col>
+                <v-col cols="12" sm="12">
+                    <v-card flat color="transparent" class="d-flex justify-center">
+                        <v-text-field placeholder="Enter Your Email" rounded class="shrink" background-color="grey" filled
+                            append-icon="mdi-email-outline" dense></v-text-field>
+                    </v-card>
+                </v-col>
             </v-row>
         </v-card>
     </v-container>
